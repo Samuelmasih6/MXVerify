f9aa96b382ae9dd1f3e1e3936d84c93373490b98
Samuel Masih <sidhusamuel567@gmail.com>
2026-07-23 00:22:28 +0530

    lookup txt dmarc, print final results

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -13,11 +14,15 @@ func main() {
 	fmt.Println("domain,hasMX,hasSPF,SPFrecord,hasDMARC,DMARCrecord")
 
 	for scanner.Scan() {
-		checkdomain(scanner.Text())
+		domain := strings.TrimSpace(scanner.Text())
+		if domain == "" {
+			continue
+		}
+		checkdomain(domain)
 	}
 
 	if err := scanner.Err(); err != nil {
-		log.Fatal("Error: could not read from input: %v \n", err)
+		log.Fatalf("Error: could not read from input: %v", err)
 	}
 }
 
@@ -39,4 +44,26 @@ func checkdomain(domain string) {
 	if err != nil {
 		log.Printf("Error: %v\n", err)
 	}
+	for _, record := range txtRecords {
+		if strings.HasPrefix(record, "v=spf1") {
+			hasSPF = true
+			SPFrecord = record
+			break
+		}
+	}
+
+	dmarcRecords, err := net.LookupTXT("_dmarc." + domain)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+
+	for _, record := range dmarcRecords {
+		if strings.HasPrefix(record, "v=DMARC1") {
+			hasDMARC = true
+			DMARCrecord = record
+			break
+		}
+	}
+	fmt.Printf("%v,%v,%v,%q,%v,%q\n", domain, hasMX, hasSPF, SPFrecord, hasDMARC, DMARCrecord)
+
 }
