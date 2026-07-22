dd745e96ee7a974b6f78fe0d51905cb1d94a3b1e
Samuel Masih <sidhusamuel567@gmail.com>
2026-07-22 23:48:51 +0530

    command line augmentation and error handling

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("domain,hasMX,hasSPF,SPFrecord,hasDMARC,DMARCrecord")
+
+	for scanner.Scan() {
+		checkdomain(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal("Error: could not read from input: %v \n", err)
+	}
 }
 
 func checkdomain(domain string) {
